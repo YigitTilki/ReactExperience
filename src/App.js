0e1d4872aa9185444afb21cf1f3791c189d48eff
Yigit Tilki <bestnemesis1234@gmail.com>
2023-03-28 03:12:19 +0300

    React Öğrenmece

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const test = 59;
+  const isAuth = true;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h4>{7+9}</h4>
+       <h4>{"Yiğit".toUpperCase()}</h4>
+       <h4>{test}</h4>
+       <div>
+        {
+          isAuth ? <p>Kullanıcı Kayıtlı</p>
+          : null
+        }
+       </div>
+        </div>
   );
 }
 
