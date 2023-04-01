1ad4cb103a91363209160e6c7eb9f093d2aa50d8
Yigit Tilki <bestnemesis1234@gmail.com>
2023-04-01 14:23:52 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 
 import './App.css';
 import React from 'react';
-import User from './components/User';
-import NavBar from './NavBar';
+import ExpenseItem from './components/ExpenseItem';
+
 
 function App() {
+
+  const expenses = [
+    {
+      id: "e1",
+      title: "toilet paper",
+      amount: 94.2,
+      date: new Date(2022, 7, 14)
+
+    },
+    {
+      id: "e2",
+      title: "car insurance",
+      amount: 450.2,
+      date: new Date(2022, 7, 14)
+
+    },
+    {
+      id: "e3",
+      title: "new phone",
+      amount: 700.25,
+      date: new Date(2022, 7, 14)
+
+    },
+    {
+      id: "e4",
+      title: "new desktop",
+      amount: 120.50,
+      date: new Date(2022, 7, 14)
+
+    }
+  ];
+
+
   return (
-    <div className="container">
-       <NavBar title='User App'/>
-       <hr />
-       <User
-       name = "Yiğit Tilki"
-       salary = "10000$"
-       department = "Computer Engineering"
-       
-       />
-       <User
-       name = "Birol Tilki"
-       department = "Head Teacher"
-       
-       />
-        </div>
+    <div>
+      <h1>First Application</h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
+
+    </div>
   );
 }
 
